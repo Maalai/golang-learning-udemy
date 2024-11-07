4099bdaa776ac57bcb560c4528a9964ab629d078
Maalaiappan Subramanian <maalai@Maalaiappans-MacBook-Pro.local>
2024-11-07 22:28:53 +0530

    structs module updates

--- structs/structs.go
+++ structs/structs.go
@@ -12,14 +12,34 @@ type User struct {
 	createdAt time.Time
 }
 
+// Receiver function of User struct
+func (u User) printUser() {
+	fmt.Printf("User name is %v %v\n", u.firstName, u.lastName)
+	fmt.Println("Birthdate is (DD/MM/YYYY): ", u.birthdate)
+}
+
+// Receiver function of User struct with pass by reference
+func (u *User) clearUser() {
+	u.birthdate = ""
+}
+
+// Utility finction to create a new instance of this User struct
+func newUser(firstName, lastName, birthdate string) *User {
+	return &User{firstName: firstName, lastName: lastName, birthdate: birthdate, createdAt: time.Now()}
+}
+
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (DD/MM/YYYY): ")
 
-	user := User{firstName: firstName, lastName: lastName, birthdate: birthdate, createdAt: time.Now()}
+	user := newUser(firstName, lastName, birthdate)
 
-	outputUser(&user)
+	user.printUser()
+
+	user.clearUser()
+
+	user.printUser()
 
 }
 
@@ -28,10 +48,4 @@ func getUserData(text string) string {
 	var value string
 	fmt.Scan(&value)
 	return value
-}
-
-func outputUser(user *User) {
-	fmt.Printf("User name is %v %v\n", user.firstName, user.lastName)
-	fmt.Println("Birthdate is (DD/MM/YYYY): ", user.birthdate)
-
 }
